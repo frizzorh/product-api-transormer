59398e9c11fc55c77c49c25fe82318b062b72b88
frossi <frossi@redhat.com>
2024-01-24 17:59:09 +0100

    simplified properties usage

--- src/main/java/org/acme/productapitransormer/Application.java
+++ src/main/java/org/acme/productapitransormer/Application.java
@@ -1,20 +1,16 @@
 package org.acme.productapitransormer;
 
-import jakarta.enterprise.context.ApplicationScoped;
 import org.apache.camel.builder.RouteBuilder;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
 
-import static org.apache.camel.component.rest.RestConstants.REST_HTTP_QUERY;
-
-@ApplicationScoped
 public class Application extends RouteBuilder {
 
     @Override
     public void configure() {
         from("rest:get:top-10-products")
                 .to("direct:cleanupHttp")
-                .setHeader(REST_HTTP_QUERY, () -> "limit=10&page=1")
-                .to("rest:get:{{services.privateProductCatalogAPIPath}}?host={{services.privateProductCatalogAPIHost}}")
+                .to("rest:get:{{services.privateProductCatalogAPIPath}}" +
+                        "?host={{services.privateProductCatalogAPIHost}}" +
+                        "&queryParameters=limit=10&page=1")
                 .to("jsonata:transformer.jsonata?inputType=JsonString&outputType=JsonString")
         ;
 
