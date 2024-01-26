5ae0d15a7b32a41e52401005f0c6445c08a0af28
frossi <frossi@redhat.com>
2024-01-26 15:12:52 +0100

    minor fix on query param

--- src/main/java/org/acme/productapitransormer/Application.java
+++ src/main/java/org/acme/productapitransormer/Application.java
@@ -10,7 +10,7 @@ public class Application extends RouteBuilder {
                 .to("direct:cleanupHttp")
                 .to("rest:get:{{services.privateProductCatalogAPIPath}}" +
                         "?host={{services.privateProductCatalogAPIHost}}" +
-                        "&queryParameters=limit=10&amppage=1")
+                        "&queryParameters=limit=10&amp;page=1")
                 .to("jsonata:transformer.jsonata?inputType=JsonString&outputType=JsonString")
         ;
 
